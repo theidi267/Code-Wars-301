4f3f7a2c56227c17b5ee6549f39bb49c5d20be3d
Timea Heidenreich <timea.heidenreich@gmail.com>
2018-04-12 18:37:00 -0700

    i don't know why github doesn't work

--- innsert-dashes/solution.js
+++ innsert-dashes/solution.js
@@ -17,3 +17,21 @@ function insertDash(num) {
   return newArray.join('');
 
 }
+
+
+// OR
+
+function insertDash(num) {
+
+  let numArray = Array.from(String(num), Number);
+
+  numArray.forEach((Number, i) => {
+
+    if(numArray[i] % 2 > 0 && numArray [i+1] %2 > 0){
+
+      numArray.splice([i+1], 0, '-' );
+    }
+  });
+  return numArray.join('');
+
+}
